da0ab8aedb78479664324449ca7dc1ebe5ace466
Sergey Telnov <s.telnov1993@mail.ru>
2022-04-08 17:27:30 +0300

    deployment

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const chalk = require("chalk");
 const cors = require("cors");
 const initDatabase = require("./startUp/initDatabase");
 const routes = require("./routes");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -14,11 +15,13 @@ app.use("/api", routes);
 
 const PORT = config.get("port") ?? 8080;
 
-// if (process.env.NODE_ENV === "production") {
-//   console.log("p");
-// } else {
-//   console.log("d");
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client")));
+  const indexPath = path.join(__dirname, "client", "index.html");
+  app.get("*", (req, res) => {
+    res.sendFile(indexPath);
+  });
+}
 
 async function start() {
   try {
